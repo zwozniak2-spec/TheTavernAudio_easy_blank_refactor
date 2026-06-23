d444bc5e74ce3a398d8da269accb5800305a3821
zwozniak2 <zwozniak2@edu.cdv.pl>
2026-06-23 20:15:18 +0200

    Dopasowanie kodu do zmiany klawiszy.
    Naciśnięcie kolejno klawiszy U, I, O, P powoduje zmianę dźwięków lub ich całkowite wyciszenie.

--- Assets/Scripts/VCA.cs
+++ Assets/Scripts/VCA.cs
@@ -26,10 +26,10 @@ public class VCA : MonoBehaviour
     void Start()
     {
         // Pobiera VCAs z FMOD.
-        globalVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Mute");
+        globalVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Global");
         musicVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Music");
-        tavernVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Tavern_amb");
-        outsideVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Outside_amb");
+        tavernVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Tavern");
+        outsideVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Outside");
 
         // Ustawia początkową głośność.
         globalVCA.setVolume(DecibelToLinear(-100));
